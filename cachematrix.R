f270aaaff855893080cf18b676b54554414b4b0c
Mark Solberger <mountain_dude@hotmail.com>
2014-12-15 22:22:20 +0100

    Update cachematrix.R
    Commit of first draft of assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix takes a matrix as an argument and creates a function object (list) containing
+## functions to:
+## create (set) the matrix
+## retreive (get) the matrix
+## store (set) the calculated inverse of the matrix 
+## retreive (get) the calculcated inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+      inverseOfMatrix <- NULL
+      set <- function(matrixToSet) {
+            x <<- matrixToSet
+            inverseOfMatrix <<- NULL
+      }
+      get <- function() {
+            return(x)
+      }
+      setinverse <- function(solvedInverseOfMatrix) { 
+            inverseOfMatrix <<- solvedInverseOfMatrix 
+      }
+      getinverse <- function() { 
+            return(inverseOfMatrix)
+      }
+      list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve takes a matrix function object and evaluates if an inverse has been calculated (set) by first
+## retreiving the inverse function value and if this is not null, return the function value back to the caller.
+## If the inverse function value was null, the function calculates the inverse by solve(matrix) 
+## and sets the inverse value on the matrix object
+cacheSolve <- function(x, ...){
+      inverseOfMatrix <- x$getinverse()
+      if(!is.null(inverseOfMatrix)) {
+            message("getting cached inverse of matrix")
+            return(inverseOfMatrix)
+      }
+      matrix <- x$get()
+      inverseOfMatrix <- solve(matrix, ...)
+      x$setinverse(inverseOfMatrix)      
+      return(inverseOfMatrix)
 }
