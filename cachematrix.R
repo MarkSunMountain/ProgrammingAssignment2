2848d8216072b3f620b9149f3097fa8f52a599e7
Mark Solberger <mountain_dude@hotmail.com>
2014-12-16 15:03:03 +0100

    Modification of text and function composition

--- cachematrix.R
+++ cachematrix.R
@@ -1,39 +1,30 @@
 ## makeCacheMatrix takes a matrix as an argument and creates a function object (list) containing
 ## functions to:
-## create (set) the matrix
+## create (set) the matrix (initializes the matrix value and sets the calculated inverse of the matrix to NULL
 ## retreive (get) the matrix
 ## store (set) the calculated inverse of the matrix 
 ## retreive (get) the calculcated inverse of the matrix
-makeCacheMatrix <- function(x = matrix()) {
-      inverseOfMatrix <- NULL
+makeCacheMatrix <- function(x = matrix()) {      
+      inverseOfMatrix <- NULL      
       set <- function(matrixToSet) {
             x <<- matrixToSet
             inverseOfMatrix <<- NULL
-      }
-      get <- function() {
-            return(x)
-      }
-      setinverse <- function(solvedInverseOfMatrix) { 
-            inverseOfMatrix <<- solvedInverseOfMatrix 
-      }
-      getinverse <- function() { 
-            return(inverseOfMatrix)
-      }
-      list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+      }      
+      get <- function() { return(x) }      
+      setinverse <- function(calculatedInverseOfMatrix) { inverseOfMatrix <<- calculatedInverseOfMatrix }      
+      getinverse <- function() { return(inverseOfMatrix) }      
+      return(list(set = set, get = get, setinverse = setinverse, getinverse = getinverse))
 }
 
 ## cacheSolve takes a matrix function object and evaluates if an inverse has been calculated (set) by first
-## retreiving the inverse function value and if this is not null, return the function value back to the caller.
-## If the inverse function value was null, the function calculates the inverse by solve(matrix) 
-## and sets the inverse value on the matrix object
+## retreiving the inverse matrix value. If this value is null, the function calculates the inverse by solve(matrix) 
+## and sets the inverse value on the matrix function object. The function then returns the inverse value.
 cacheSolve <- function(x, ...){
       inverseOfMatrix <- x$getinverse()
-      if(!is.null(inverseOfMatrix)) {
-            message("getting cached inverse of matrix")
-            return(inverseOfMatrix)
+      if(is.null(inverseOfMatrix)) {       
+            matrix <- x$get()
+            inverseOfMatrix <- solve(matrix, ...)
+            x$setinverse(inverseOfMatrix)            
       }
-      matrix <- x$get()
-      inverseOfMatrix <- solve(matrix, ...)
-      x$setinverse(inverseOfMatrix)      
       return(inverseOfMatrix)
 }
